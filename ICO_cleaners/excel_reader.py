3d6c7e266c4f2d72d63b4b1c5ef98f5e6bd88dfe
Hiru <zcabhra@ucl.ac.uk>
2018-12-13 00:18:28 +0000

    tried to make a method to clean the xlsv files

--- ICO_cleaners/excel_reader.py
+++ ICO_cleaners/excel_reader.py
@@ -9,31 +9,51 @@ import json
 disappearance = pd.read_excel('DisappearanceCalendarYear1963-2016.xlsx', header=5)
 domestic_consumption = pd.read_excel('DomesticConsumptionCalendarYear1963-2016.xlsx', header=5)
 
-years = list(disappearance.columns.values)[1:]
+# years = list(disappearance.columns.values)[1:]
 
-# TODO convert this repetitive for loop into a cleaner method
-disappearance_dictionary = {}
-for country in disappearance.filter(regex="Country").values:
-    country = country[0]
-    disappearance_vals = disappearance.loc[disappearance['Importing Country'] == country].values.tolist()[0][1:]
-    disappearance_dictionary[country] = dict(zip(years, disappearance_vals))
+# disappearance_dictionary = {}
+# for country in disappearance.filter(regex="Country").values:
+#     country = country[0]
+#     disappearance_vals = disappearance.loc[disappearance['Importing Country'] == country].values.tolist()[0][1:]
+#     disappearance_dictionary[country] = dict(zip(years, disappearance_vals))
+#
+# domestic_consumption_dictionary = {}
+# for country in [y for x in domestic_consumption.filter(regex="Country").values for y in x]:
+#     # format and generate an array of values for each country each year
+#     country_data_as_array = np.array(domestic_consumption.loc[domestic_consumption['Country'] == country].values.tolist()[0][2:])
+#     # change all NaNs to 0s
+#     domestic_consumption_values = np.nan_to_num(country_data_as_array).astype(int).tolist()
+#     # bind each value to its respective year
+#     domestic_consumption_dictionary[country] = dict(zip(years, domestic_consumption_values))
+# data = {"Domestic Consumption": domestic_consumption_dictionary}
 
-domestic_consumption_dictionary = {}
-for country in [y for x in domestic_consumption.filter(regex="Country").values for y in x]:
-    # format and generate an array of values for each country each year
-    country_data_as_array = np.array(domestic_consumption.loc[domestic_consumption['Country'] == country].values.tolist()[0][2:])
-    # change all NaNs to 0s
-    domestic_consumption_values = np.nan_to_num(country_data_as_array).astype(int).tolist()
-    # bind each value to its respective year
-    domestic_consumption_dictionary[country] = dict(zip(years, domestic_consumption_values))
+
+# TODO sort out the coffee type conflict causing years to not be correctly assigned
+def ico_excel_formatter(excel_file, data_title):
+    df = pd.read_excel(excel_file, header=5)
+    # Get country column name
+    column_name = df.filter(regex="Country").columns.values[0]
+    years = list(df.columns.values)[2:]
+    ### print(years) ###
+    df_dictionary = {}
+    for country in df[column_name]:
+        ### print(len(df.loc[df[column_name] == country].values.tolist()[0])) ###
+        # format and generate an array of values for each country each year
+        country_data_as_array = np.array(
+            df.loc[df[column_name] == country].values.tolist()[0][2:])
+        # change all NaNs to 0s
+        df_values = np.nan_to_num(country_data_as_array).astype(int).tolist()
+        # bind each value to its respective year
+        df_dictionary[country] = dict(zip(years, df_values))
+    return {data_title: df_dictionary}
 
 
-print(domestic_consumption.loc[domestic_consumption['Country'] == 'Bolivia'])
+print(ico_excel_formatter('DomesticConsumptionCalendarYear1963-2016.xlsx', 'Data_TEST'))
 
 ''' 
 automated scraper should look into DB find the most recent year that there is data for, compare it against the new excel 
 file that it downloads and only extract and clean new data that it finds
 '''
 
-with open('ICO.json', 'w') as output:
-    json.dump(disappearance_dictionary, output)
+# with open('ICO.json', 'w') as output:
+#     json.dump(data, output)
