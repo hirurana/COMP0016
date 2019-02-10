d44f02b8f6d241fec89a752936c1e0be92bfdd54
Yiota <yiota.kr@gmail.com>
2019-02-10 17:37:11 +0200

    updated index.js file

--- Yiota/investmentplatform/routes/index.js
+++ Yiota/investmentplatform/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/list', function(req, res){
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://localhost:27017/coffee';
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      console.log('Cannot connect to server', err);
+    } else {
+      console.log("Successfully connected to server");
+      var collection = db.collection('name'); //put correct name of collection
+
+      collection.find({}).toArray(function (err, result) {
+      if (err) {
+        res.send(err);
+      } else if (result.length) {
+        res.render('articles',{
+        "articles" : result
+        });
+      } else {
+        res.send('Nothing found');
+      }
+      db.close();
+    });
+  }
+  });
+});
 module.exports = router;
