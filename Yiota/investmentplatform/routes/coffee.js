a9ce0e3955edad92c402b26013652939f883eb38
Yiota <yiota.kr@gmail.com>
2019-02-13 15:59:45 +0200

    updated coffee page

--- Yiota/investmentplatform/routes/coffee.js
+++ Yiota/investmentplatform/routes/coffee.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET coffee page. */
 router.get('/', function(req, res, next) {
-  res.send('coffee page');
+  res.render('coffee', { title: 'Investment Platform - Coffee' });
 });
 
 module.exports = router;
