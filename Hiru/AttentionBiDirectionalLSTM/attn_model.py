5950c3d475cbbbb2426dfaa99bf5727d82605172
Hiru <zcabhra@ucl.ac.uk>
2019-03-10 17:55:53 +0000

    analysed news articles and sorted model

--- Hiru/AttentionBiDirectionalLSTM/attn_model.py
+++ Hiru/AttentionBiDirectionalLSTM/attn_model.py
@@ -1,81 +1,37 @@
 import tensorflow as tf
 from keras import layers, Input, Model
-from keras.backend import zeros
 from keras.callbacks import EarlyStopping
-from keras.layers import multiply, Embedding, Bidirectional, LSTM, Dense, Flatten, Activation, RepeatVector, Permute, K, \
-    np
+from keras.layers import multiply, Embedding, Bidirectional, LSTM, Dense, Flatten, Activation, RepeatVector, Permute, K
 from keras.optimizers import Adam
 from keras_preprocessing import sequence
 from keras_preprocessing.text import Tokenizer
-from numpy import asarray
+import imdb
+
+imdb.maybe_download_and_extract()
 
 vocab_size = 20000
 
-pad_id = 0
-start_id = 1
-oov_id = 2
-index_offset = 2
+x_train, y_train = imdb.load_data(train=True)
+x_test, y_test = imdb.load_data(train=False)
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(num_words=vocab_size, start_char=start_id,
-                                                                        oov_char=oov_id, index_from=index_offset)
-print(x_train[1])
 data_text = x_train + x_test
 
-####################### CHANGE TO GLOVE ##########################
-word2idx = tf.keras.datasets.imdb.get_word_index()
+# ####################### CHANGE TO GLOVE ##########################
+tokenizer = Tokenizer(num_words=vocab_size)
+tokenizer.fit_on_texts(data_text)
 
-idx2word = {v + index_offset: k for k, v in word2idx.items()}
+# integer encode the documents
+x_train = tokenizer.texts_to_sequences(x_train)
+x_test = tokenizer.texts_to_sequences(x_test)
 
-idx2word[pad_id] = '<PAD>'
-idx2word[start_id] = '<START>'
-idx2word[oov_id] = '<OOV>'
-
-max_len = 250
-rnn_cell_size = 512
+# pad documents to a max length of 500 words
+max_len = 500
+rnn_cell_size = 128
 
 pad_type = 'pre'
 
-x_train = sequence.pad_sequences(x_train, maxlen=max_len, truncating=pad_type, padding=pad_type, value=pad_id)
-x_test = sequence.pad_sequences(x_test, maxlen=max_len, truncating=pad_type, padding=pad_type, value=pad_id)
-
-####################### CHANGE TO GLOVE ##########################
-
-# tokenizer = Tokenizer()
-# tokenizer.fit_on_texts(data_text)
-#
-# if vocab_size is None:
-#     vocab_size = len(tokenizer.word_index) + 1
-#
-# # integer encode the documents
-# x_train = tokenizer.texts_to_sequences(x_train)
-# x_test = tokenizer.texts_to_sequences(x_test)
-#
-# # pad documents to a max length of 250 words
-# max_len = 250
-# rnn_cell_size = 128
-#
-# pad_type = 'pre'
-#
-# x_train = sequence.pad_sequences(x_train, maxlen=max_len, truncating=pad_type, padding=pad_type, value=pad_id)
-# x_test = sequence.pad_sequences(x_test, maxlen=max_len, truncating=pad_type, padding=pad_type, value=pad_id)
-#
-# embeddings_index = dict()
-# f = open('glove.6B.100d.txt')
-# for line in f:
-#     values = line.split()
-#     word = values[0]
-#     coefs = tf.convert_to_tensor(asarray(values[1:], dtype='float32'), dtype='float32')
-#     embeddings_index[word] = coefs
-# f.close()
-# print('Loaded %s word vectors.' % len(embeddings_index))
-#
-# # create a weight matrix for words in training docs
-# embedding_matrix = zeros((vocab_size, 100))
-# for word, i in tokenizer.word_index.items():
-#     embedding_vector = embeddings_index.get(word)
-#     if embedding_vector is not None:
-#         embedding_matrix[i] = embedding_vector
-
+x_train = sequence.pad_sequences(x_train, maxlen=max_len, truncating=pad_type, padding=pad_type)
+x_test = sequence.pad_sequences(x_test, maxlen=max_len, truncating=pad_type, padding=pad_type)
 
 ###################################################################
 
@@ -113,8 +69,6 @@ history = model.fit(x_train,
                     y_train,
                     epochs=10,
                     batch_size=200,
-                    validation_split=.3, verbose=1, callbacks=[early_stopping_callback])
+                    validation_split=0.05, verbose=1, callbacks=[early_stopping_callback])
 
 model.save('attention_weights.h5')
-
-# 5 fold cross validation
